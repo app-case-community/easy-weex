bb78cc429d47368f25306bfaa295266e23fddb73
zhuzhe <zhuzhe@findpiano.cn>
2018-12-01 18:01:56 +0800

    add f2

--- src/views/test.vue
+++ src/views/test.vue
@@ -3,11 +3,59 @@
     <div class="panel">
       <text class="text">Weex 是一套简单易用的跨平台开发方案，能以 Web 的开发体验构建高性能、可扩展的原生应用。</text>
     </div>
-    <!-- <lottie style='width:200px;height:200px;border-width:2px;' loop=true src="local://lottie/coinpig.json" />
-    <lottie style='width:200px;height:200px;border-width:2px;' loop=true src="local://lottie/motorcycle.json" /> -->
-    <lottie style='width:200px;height:200px;border-width:2px;' loop=true src="https://www.lottiefiles.com/storage/datafiles/nwHJNOUoNltzK7d/data.json" />
+    <f2-chart :data="chartData" style="width:750px;height:600px;" />
+    <!-- <text>本地资源1</text>
+    <lottie style='width:200px;height:200px;border-width:2px;' loop=true src="local://lottie/coinpig.json" />
+    <text>本地资源1</text>
+    <lottie style='width:200px;height:200px;border-width:2px;' loop=true src="local://lottie/preloader.json" /> -->
+    <text>在线资源</text>
+    <lottie ref="lottie3" style='width:200px;height:200px;border-width:2px;' @loaded="loadFinish" loop=true src="https://www.lottiefiles.com/storage/datafiles/nwHJNOUoNltzK7d/data.json" />
   </scroller>
 </template>
+<script>
+  import F2Chart from '@components/f2-chart.vue'
+  export default {
+    components: {
+      F2Chart
+    },
+    data () {
+      return {
+        chartData: [{
+                      genre: 'Sports',
+                      sold: 275
+                    },
+                    {
+                      genre: 'Strategy',
+                      sold: 115
+                    },
+                    {
+                      genre: 'Action',
+                      sold: 120
+                    },
+                    {
+                      genre: 'Shooter',
+                      sold: 350
+                    },
+                    {
+                      genre: 'Other',
+                      sold: 150
+                    }
+        ]
+      }
+    },
+    methods: {
+      loadFinish () {
+        this.$refs.lottie3.play()
+        setTimeout(() => {
+          this.$refs.lottie3.pause()
+          setTimeout(() => {
+            this.$refs.lottie3.reset()
+          }, 1000)
+        }, 3000)
+      }
+    }
+  }
+</script>
 
 <style scoped>
 .panel {
